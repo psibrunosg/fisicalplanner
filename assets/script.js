13cf935df5b1adc28aac144462aa9f96f0c6da11
Bruno Souza <130676240+psibrunosg@users.noreply.github.com>
2026-02-03 19:53:56 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         try {
             // 1. Busca a lista de usuários
-            const response = await fetch('/users.json');
+            const response = await fetch('https://psibrunosg.github.io/fisicalplanner/user/users.json');
             const users = await response.json();
 
             // 2. Verifica se existe alguém com esse email e senha
